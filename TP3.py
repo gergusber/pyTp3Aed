13e0f7b9371b465704dd94943f317d2311fcb906
German Bertea <gerbertea@gmail.com>
2018-09-18 19:52:27 -0300

    first commit

---
+++ TP3.py
@@ -0,0 +1,112 @@
+
+#Declaración de un registro con su constructor.
+class Canciones:
+    #Constructor
+    def __init__(self, tit = 'asignar', alb = 'asignar', ban = 'asignar', lang = 0, gen = 0, repr = 0):
+        self.titulo = tit
+        self.album = alb
+        self.banda = ban
+        self.idioma = lang
+        self.genero = gen
+        self.reproducciones = repr
+
+
+def write(canciones):
+    print('Titulo:', canciones.titulo, end=' ')
+    print('Album:', canciones.album, end=' ')
+    print('Banda:', canciones.banda, end=' ')
+    print('Idioma:', canciones.idioma, end=' ')
+    print('Genero:', canciones.genero, end=' ')
+    print('Reproducciones:', canciones.reproducciones, end=' ')
+    print("")
+
+def validate(inf):
+    n = inf
+    while n <= inf:
+        n = int(input('Ingrese cantidad de canciones a cargar (mayor a ' + str(inf) + ' por favor): '))
+        if n <= inf:
+            print('Error: se pidio mayor a', inf, '... cargue de nuevo...\n'
+                                                  '>>>Ingrese: ')
+
+    return n
+
+
+def read(canciones):
+    n = len(canciones)
+    for i in range(n):
+        lan = -1
+        gen = -1
+        repr = -1
+        tit = input('Título de la canción: ')
+        alb = input('Nombre del álbum: ')
+        ban = input('Nombre de la banda: ')
+        while lan < 0 or lan > 3 :
+            lan = int(input('Ingrese el idioma del 0 al 3: '))
+        while gen < 0 or gen > 9:
+            gen = int(input('Ingrese el género del 0 al 9: '))
+        while repr < 0:
+            repr = int(input('Ingrese la cantidad de reproducciones: '))
+
+        print()
+
+        canciones[i] = Canciones(tit, alb, ban, lan, gen, repr)
+
+
+
+def sort(canciones):
+    n = len(canciones)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if canciones[i].titulo > canciones[j].titulo:
+                canciones[i], canciones[j] = canciones[j], canciones[i]
+
+
+def display(canciones):
+    n = len(canciones)
+    print('\nCanciones ordenadas alfabeticamente: ')
+    for i in range(n):
+        write(canciones[i])
+
+def display_Test(canciones, x):
+    n = len(canciones)
+    print('Canciones con cantidad de reproducciones >=', x, '):')
+    for i in range(n):
+        if canciones[i].reproducciones >= x:
+            write(canciones[i])
+
+
+
+def test():
+    # cargar cantidad de canciones
+    n = validate(0)
+
+    canciones = n * [None]
+
+    # cargar el arreglo por teclado...
+    print('\nCargue los datos de las canciones:')
+    read(canciones)
+    print()
+
+
+    opcion = 0
+    while opcion < 1 or opcion > 8:
+        opcion = int(input("ERROR /// Se pidio que ingrese entre 1 y 8.\n>>>Ingrese:  "))
+
+        if opcion == 1:
+            display(canciones)
+
+
+
+
+
+
+
+   # x = int(input("Ingrese para visualizar las canciones con >= reproducciones que: "))
+
+
+    #sort(canciones)
+
+   # display(canciones)
+
+
+test()
